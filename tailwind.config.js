29d6ffa33014675f863c1430e9856b12ad6903c9
Farhad Nawab <farhadnawab.official@gmail.com>
2020-11-27 13:49:47 +0000

    header responsive changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,18 @@ module.exports = {
       fontFamily: {
         'europa': 'europa-regular',
         'tenor': 'Tenor Sans'
-       }
+      }, 
+      fontSize: {
+        '6xl': '3.81rem',
+        '7xl': '4.35rem',
+        '8xl': '5.5rem',
+        '1-5rem': '1.5rem',
+        '2rem': '2rem',
+        '2-5rem': '2.5rem',
+        '2-75rem': '2.75rem',
+        '3-5rem': '3.5rem',
+        '4rem': '4rem'
+      }
     },
   },
   variants: {
