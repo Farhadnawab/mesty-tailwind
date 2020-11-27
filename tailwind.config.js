cfc2ba4a89dba0936b6ba219184098229a9a6f75
Farhad Nawab <farhadnawab.official@gmail.com>
2020-11-27 13:05:07 +0000

    header, footer & other sections basic html created

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#d5814c',
+        secondary: '#ffdb8a',
+        dark: '#231f20'
+      },
+      fontFamily: {
+        'europa': 'europa-regular',
+        'tenor': 'Tenor Sans'
+       }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
